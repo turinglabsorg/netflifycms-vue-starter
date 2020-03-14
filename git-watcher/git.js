0505dffc0d6201c64eb59e0e446f40c9446d8c48
turinglabsorg <turinglabs@criptext.com>
2020-03-14 06:19:23 +0100

    build script

--- git-watcher/git.js
+++ git-watcher/git.js
@@ -1,11 +1,13 @@
 const git = require('simple-git')('../');
+const path = require('path')
+const script = path.join(__dirname, 'build.sh');
 
 function watch(){
     git.exec(() => console.log('starting pull...'))
     .pull((err, update) => {
     if(update && update.summary.changes) {
             console.log('updating vue build...')
-            require('child_process').exec('cd .. & npm run build')
+            require('child_process').exec(script, function(error, stdout, stderr){ if(error){console.log(error)} });
             console.log('vue built.')
             setTimeout(function(){
                 watch()
